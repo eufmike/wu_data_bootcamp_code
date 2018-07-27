ee1fe52fd96094d04044c5c2fae573b58a33c3bb
Michael Shih <eufmike@users.noreply.github.com>
2018-07-26 23:41:50 -0500

    done

--- homework/w5_Pymaceuticals/code.py
+++ homework/w5_Pymaceuticals/code.py
@@ -45,25 +45,6 @@ timepoint = list(data_avg.index.levels[1])
 # plot
 ax = plt.subplot(111)
 for i in drug:
-    ax.errorbar(x = timepoint, y = data_avg.loc[i, 'Metastatic Sites'], 
-                 yerr = data_sem.loc[i, 'Metastatic Sites'], 
-                 capsize = 3, marker='o'
-                 )
-ax.legend(drug, loc=9, bbox_to_anchor=(0.5, -0.15), ncol = 2)
-ax.grid(True)
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
-ax.set_xlim(-3, 48)
-ax.set_title('Metastatic Spread During Treatment')
-ax.set_xlabel('Timepoint (day)')
-ax.set_ylabel('Metastatic Site(s)')
-plt.show()
-
-# %%
-# Metastatic Spread During Treatment
-# plot
-ax = plt.subplot(111)
-for i in drug:
     ax.errorbar(x = timepoint, y = data_avg.loc[i, 'Tumor Volume (mm3)'], 
                  yerr = data_sem.loc[i, 'Tumor Volume (mm3)'], 
                  capsize = 3, marker='o'
@@ -79,12 +60,31 @@ ax.set_ylabel('Tumor Volume ($mm^3$)')
 plt.show()
 
 # %%
+# Metastatic Spread During Treatment
+# plot
+ax = plt.subplot(111)
+for i in drug:
+    ax.errorbar(x = timepoint, y = data_avg.loc[i, 'Metastatic Sites'], 
+                 yerr = data_sem.loc[i, 'Metastatic Sites'], 
+                 capsize = 3, marker='o'
+                 )
+ax.legend(drug, loc=9, bbox_to_anchor=(0.5, -0.15), ncol = 2)
+ax.grid(True)
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.set_xlim(-3, 48)
+ax.set_title('Metastatic Spread During Treatment')
+ax.set_xlabel('Timepoint (day)')
+ax.set_ylabel('Metastatic Site(s)')
+plt.show()
+
+# %%
 # Survival chart
 # return max mouse count
 data_count = data.groupby(['Drug', 'Timepoint'])['Mouse ID'].count()
-data_count
 mouse_max_count = list(data_count.groupby('Drug').max())
-
+data_count
+# %%
 # map 1st day count to a new column
 maxcountmatch = dict(zip(drug, mouse_max_count))
 data['max'] = data['Drug'].map(maxcountmatch)
@@ -106,7 +106,7 @@ data_count_max.head(50)
 # make plot
 ax = plt.subplot(111)
 for i in drug:
-    ax.plot(timepoint, data_count_max.loc[i, 'percentage'], '-o')
+    ax.step(x = timepoint, y = data_count_max.loc[i, 'percentage'], linestyle = '-', where = 'post')
     # plt.plot(x = timepoint, y = data_count_max.loc[i, 'percentage'], lw=3)
 ax.set_xlim(-3, 48)
 ax.set_ylim(0, 105)
@@ -118,3 +118,48 @@ ax.set_title('Survival During Treatment')
 ax.set_xlabel('Timepoint (day)')
 ax.set_ylabel('Servival Rate (%)')
 plt.show()
+
+# %%
+# Tumor change over 45 treatment
+data.head(50)
+data_day0 = data[data['Timepoint'] == 0]
+data_day0.head(50)
+data_day45 = data[data['Timepoint'] == 45]
+result = pd.merge(data_day0[['Mouse ID', 'Tumor Volume (mm3)', 'Drug']], 
+                data_day45[['Mouse ID', 'Tumor Volume (mm3)', 'Drug']], 
+                on = ['Mouse ID', 'Drug']
+                )
+
+result['Tumor_change_%'] = (result['Tumor Volume (mm3)_y'] - result['Tumor Volume (mm3)_x']) / result['Tumor Volume (mm3)_x'] * 100
+result_avg = result.groupby('Drug')['Tumor_change_%'].agg({'Tumor_change_%': 'mean'})
+result_avg
+
+#%%
+# check if the changes smaller then zero
+result_avg['smaller_then_zero'] = np.where((result_avg['Tumor_change_%'] <= 0), 'g', 'r')
+result_avg
+
+# %%
+# plot
+fig, ax = plt.subplots()
+
+plot = ax.bar(x = drug, height = result_avg['Tumor_change_%'], width = 0.9, color = result_avg['smaller_then_zero'])
+for x in plot:
+    height = x.get_height()
+    ax.text(x.get_x() + 0.07, 3 * abs(height)/height -0.8, '{:.0f}%'.format(height), color = 'w')
+
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
+ax.set_title('Tumor Change Over 45-Day Treatment')
+ax.set_xlabel('Treatment')
+ax.set_ylabel('Tumor Volume Change (%)')
+ax.axhline(y = 0, color = 'black', linewidth = 0.5)
+ax.yaxis.grid(True)
+ax.set_axisbelow(True)
+plt.xticks(rotation=45)
+plt.show()
+
+
+#%%
+[0]*len(drug)
