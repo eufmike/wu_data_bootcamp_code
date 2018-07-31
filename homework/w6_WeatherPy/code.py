6bdff3987ca28abb4839338d492ad35914dc5fd4
Michael Shih <sc2.shih@gmail.com>
2018-07-31 17:21:12 -0500

    build cities

--- homework/w6_WeatherPy/code.py
+++ homework/w6_WeatherPy/code.py
@@ -8,9 +8,9 @@ import time
 from citipy import citipy
 
 # office
-# path = '/Users/michaelshih/Documents/code/education/wu_data_bootcamp_code/homework/w6_WeatherPy'
+path = '/Users/michaelshih/Documents/code/education/wu_data_bootcamp_code/homework/w6_WeatherPy'
 # laptop
-path = '/Users/major_minor1982/Documents/code/Python/wu_data_bootcamp_code/homework/w6_WeatherPy'
+# path = '/Users/major_minor1982/Documents/code/Python/wu_data_bootcamp_code/homework/w6_WeatherPy'
 os.chdir(path)
 
 import api_keys
@@ -18,11 +18,29 @@ import imp
 imp.reload(api_keys)
 
 # %%
+# output file path
 outpath = "output/cities.csv"
 
-## this is develop3
-## lalala
-## test 1
+# range of latitudes and longitudes
+lat_range = (-90, 90)
+lng_range = (-180, 180)
+samplesize = 1500
 
+lat_lngs = []
+cities = []
+# Create a set of random lat and lng combination
+lats = np.random.uniform(low = lat_range[0], high = lat_range[1], size = samplesize)
+lngs = np.random.uniform(low = lng_range[0], high = lng_range[1], size = samplesize)
+'''
+scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)), random_state=seed)
+scipy.stats.uniform.rvs(loc = 0, scale = Dx, size = ((N, 1)), random_state=seed)
+'''
+lat_lngs = zip(lats, lngs)
 
+for lat_lng in lat_lngs:
+    city = citipy.nearest_city(lat_lng[0], lat_lng[1]).city_name
+    if city not in cities:
+         cities.append(city)
+
+len(cities)
 
