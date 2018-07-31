c1be9b2d21bd7024063797779a91ecd303a197cd
Michael Shih <eufmike@users.noreply.github.com>
2018-07-31 11:17:03 -0500

    good

--- homework/w6_WeatherPy/code.py
+++ homework/w6_WeatherPy/code.py
@@ -5,3 +5,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from cycler import cycler
 import time
+from citipy import citipy
+
+# office
+# path = '/Users/michaelshih/Documents/code/education/wu_data_bootcamp_code/homework/w6_WeatherPy'
+# laptop
+path = '/Users/major_minor1982/Documents/code/Python/wu_data_bootcamp_code/homework/w6_WeatherPy'
+os.chdir(path)
+
+import api_keys
+import imp
+imp.reload(api_keys)
+
+# %%
+outpath = "output/cities.csv"
+
+
