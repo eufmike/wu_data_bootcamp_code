73f3531f7f1ca55eea1c709aecfe19dfe56ca51b
Michael Shih <sc2.shih@gmail.com>
2018-09-20 17:34:39 -0500

    dd

---
+++ course/w13_2/test_06_Render.py
@@ -0,0 +1,174 @@
+# %%
+# ----------------------------------------------------------------------------------------------------------------
+# Render_Dict
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    player_dictionary = {"player_1": "Jessica",
+                         "player_2": "Mark"}
+    return render_template("index.html", dict=player_dictionary)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# %%
+# ----------------------------------------------------------------------------------------------------------------
+# Render_From_Mongo
+from flask import Flask, render_template
+
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Connect to a database. Will create one if not already available.
+db = client.team_db
+
+# Drops collection if available to remove duplicates
+db.team.drop()
+
+# Creates a collection in the database and inserts two documents
+db.team.insert_many(
+    [
+        {
+            'player': 'Jessica',
+            'position': 'Point Guard'
+        },
+        {
+            'player': 'Mark',
+            'position': 'Center'
+        }
+    ]
+)
+
+
+# Set route
+@app.route('/')
+def index():
+    # Store the entire team collection in a list
+    teams = list(db.team.find())
+    print(teams)
+
+    # Return the template with the teams list passed in
+    return render_template('index.html', teams=teams)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# %%
+# ----------------------------------------------------------------------------------------------------------------
+# Render_List
+
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def index():
+    team_list = ["Jumpers", "Dunkers", "Dribblers", "Passers"]
+    return render_template("index.html", list=team_list)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# %%
+# ----------------------------------------------------------------------------------------------------------------
+# Render_String
+# import necessary libraries
+from flask import Flask, render_template
+
+# create instance of Flask app
+app = Flask(__name__)
+
+
+# create route that renders index.html template
+@app.route("/")
+def echo():
+    return render_template("index.html", text="Serving up cool text from the Flask server!!")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# %%
+# ----------------------------------------------------------------------------------------------------------------
+# Scrape_And_Render
+# app.py
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_craigslist
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+
+
+@app.route("/")
+def index():
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
+
+
+@app.route("/scrape")
+def scraper():
+    listings = mongo.db.listings
+    listings_data = scrape_craigslist.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# scrape_craiglist.py
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    listings = {}
+
+    url = "https://raleigh.craigslist.org/search/hhh?max_price=1500&availabilityMode=0"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    listings["headline"] = soup.find("a", class_="result-title").get_text()
+    listings["price"] = soup.find("span", class_="result-price").get_text()
+    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+
+    return listings
+
