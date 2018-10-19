5bb64a897db483bf851e3ab0e7191e4e63b57aba
Michael Shih <sc2.shih@gmail.com>
2018-10-19 14:27:30 -0500

    first part done

--- homework/w17/js/logic.js
+++ homework/w17/js/logic.js
@@ -3,6 +3,7 @@ var myMap = L.map("map", {
   center: [39.50, -98.35],
   zoom: 4
 });
+//==================================================================
 
 //Create a map and fill in the container
 var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -12,37 +13,63 @@ var lightmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png
   accessToken: API_KEY
 });
 
-// We use the addTo method to add objects to our map
+//Use the addTo method to add objects to our map
 lightmap.addTo(myMap); 
 
-//create function for add earthquake bubbles
+//==================================================================
 
-
+//Create function for add earthquake bubbles
 //load data
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson';
-d3.json(url, function(data){
-  var myLayer = L.geoJSON(data);
 
+function getColor(d){
+  return d > 5 ? '#bd0026' :
+        d > 4  ? '#f03b20' :
+        d > 3  ? '#fd8d3c' :
+        d > 2  ? '#feb24c' :
+        d > 1  ? '#fed976' :
+                  '#ffffb2';
+};
+
+d3.json(url, function(data){
+  var myLayer = L.geoJSON(data, {
+    pointToLayer: function(feature, latlng) {
+
+      //Create marker options
+      var geojsonCircleOptions = {
+        radius: feature.properties.mag*7000,
+        fillColor: getColor(feature.properties.mag),
+        color: "#000",
+        weight: 0.5,
+        opacity: 1,
+        fillOpacity: 0.8
+      };      
+      
+      return L.circle(latlng, geojsonCircleOptions);
+    }
+  });
   myLayer.addTo(myMap);
+  console.log(data);
 });
 
+//==================================================================
+// Create legend
+var legend = L.control({position: 'bottomright'});
 
+legend.onAdd = function (myMap) {
 
+    var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 1, 2, 3, 4, 5],
+    labels = [];
 
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
 
-/*
-var incidents = data.features;
-    console.log(incidents);
-    var bikeMarkers = [];
-
-
-for (var index = 0; index < incidents.length; index++) {
-  var incident = incidents[index];
-
-  var bikeMarker = L.marker([incident.coordinates[0], incident.coordinates[1]]);
-  bikeMarkers.push(bikeMarker);
+    return div;
 };
-createMap(L.layerGroup(bikeMarkers));
-};
 
-*/
+legend.addTo(myMap);
